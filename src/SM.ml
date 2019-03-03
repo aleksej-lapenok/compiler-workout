a174f2642826f23ea22dfa14f0c344402b961d49
Aleksej Lapenok <live-penguin@yandex-team.ru>
2019-03-03 14:22:21 +0300

    hw3

--- src/SM.ml
+++ src/SM.ml
@@ -24,7 +24,27 @@ type config = int list * Stmt.config
 
    Takes a configuration and a program, and returns a configuration as a result
  *)                         
-let eval _ = failwith "Not yet implemented"
+let evalRow (stack, (state, cfgInput, cfgOutput))  prg = match prg with
+  | BINOP op -> (match stack with
+                 | y::x::tail -> ([Language.Expr.getFunction op x y] @ tail, (state, cfgInput, cfgOutput))
+                 | _ -> failwith "Not enouth values on stack for BINOP"
+                 )
+  | CONST n -> ([n] @ stack, (state, cfgInput, cfgOutput))
+  | READ -> (match cfgInput with
+            | head :: tail -> ([head] @ stack, (state, tail, cfgOutput))
+            | _ -> failwith "Can't read from inputStream"
+            )
+  | WRITE -> (match stack with 
+             | head :: tail -> (tail, (state, cfgInput, cfgOutput @ [head]))
+             | _ -> failwith "Not enouth values on stack for WRITE"
+             )
+  | LD x -> ([state x] @ stack, (state, cfgInput, cfgOutput))
+  | ST x -> (match stack with 
+            | head::tail -> (tail, (Language.Expr.update x head state, cfgInput, cfgOutput))
+            | _ -> failwith "Not enouth values on stack for ST"
+            )
+
+let eval cfg prg = List.fold_left evalRow cfg prg
 
 (* Top-level evaluation
 
@@ -41,4 +61,14 @@ let run p i = let (_, (_, _, o)) = eval ([], (Language.Expr.empty, i, [])) p in 
    Takes a program in the source language and returns an equivalent program for the
    stack machine
  *)
-let compile _ = failwith "Not yet implemented"
+let rec compileRow expr = match expr with
+  | Language.Expr.Const n -> [CONST n]
+  | Language.Expr.Var x -> [LD x]
+  | Language.Expr.Binop (op, a, b) -> (compileRow a) @ (compileRow b) @ [BINOP op]
+
+
+let rec compile stmt = match stmt with
+  | Language.Stmt.Read x -> [READ; ST x]
+  | Language.Stmt.Write e -> (compileRow e) @ [WRITE]
+  | Language.Stmt.Assign (x, e) -> (compileRow e) @ [ST x]
+  | Language.Stmt.Seq (stmt1, stmt2) -> (compile stmt1) @ (compile stmt2)
